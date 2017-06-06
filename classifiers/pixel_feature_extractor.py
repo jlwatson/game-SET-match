3209e55680bbfa1f1685f5595bf761d157d087db
Kent Blake <kmblake@stanford.edu>
2017-06-06 16:58:27 -0700

    Using color distances

--- classifiers/pixel_feature_extractor.py
+++ classifiers/pixel_feature_extractor.py
@@ -25,12 +25,16 @@ class PixelFeatureExtractor:
 		image = img_as_float(resized)
 		pixels = image.reshape(image.shape[0] * image.shape[1], 3)
 		filtered_pixels = np.array([p for p in pixels if 0.4 <= np.linalg.norm(p) <= 1.1])
+		mean_color = np.mean(filtered_pixels, axis=0)
+		distance_red = np.linalg.norm(mean_color - (1, 0, 0))
+		distance_green = np.linalg.norm(mean_color - (0, 1, 0))
+		distance_purple = np.linalg.norm(mean_color - (102./255, 0, 204./255))
 		if (filtered_pixels.shape[0] == 0):
 			print "No filtered pixels"
 			pdb.set_trace()
 		mean_color = np.mean(filtered_pixels, axis=0)
 		# pdb.set_trace()
-		return mean_color.flatten()
+		return np.array([distance_red, distance_green, distance_purple])
 		
 # Example usage
 # p = PixelFeatureExtractor()
