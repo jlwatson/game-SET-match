7f2bb32d9a97c0c6e2448b3e7c447d725d8c2a0e
Kent Blake <kmblake@stanford.edu>
2017-06-08 01:15:52 -0700

    Using gold color values

--- classifiers/simple_color_classifier.py
+++ classifiers/simple_color_classifier.py
@@ -11,12 +11,15 @@ COLOR_CODES = dict({'green': 0, 'purple': 1, 'red': 2})
 def get_color(img_filename):
   image = img_as_float(io.imread(img_filename))
   pixels = image.reshape(image.shape[0] * image.shape[1], 3)
-  filtered_pixels = np.array([p for p in pixels if 0.4 <= np.linalg.norm(p) <= 1.0])
+  filtered_pixels = np.array([p for p in pixels if 0.4 <= np.linalg.norm(p) <= 0.9])
   mean_color = np.mean(filtered_pixels, axis=0)
-  distance_red = np.linalg.norm(mean_color - (1, 0, 0))
-  distance_green = np.linalg.norm(mean_color - (0, 1, 0))
-  distance_purple = np.linalg.norm(mean_color - (102./255, 0, 204./255))
-  distance_blue = np.linalg.norm(mean_color - (0, 0, 1))
+  # distance_red = np.linalg.norm(mean_color - (1, 0, 0))
+  # distance_green = np.linalg.norm(mean_color - (0, 1, 0))
+  # distance_purple = np.linalg.norm(mean_color - (102./255, 0, 204./255))
+  distance_green = np.linalg.norm(mean_color - np.array([38., 169., 57.]) / 255)
+  distance_red = np.linalg.norm(mean_color - np.array([171., 25., 35.]) / 255)
+  distance_purple = np.linalg.norm(mean_color - np.array([87., 17., 152.]) / 255)
+  # distance_blue = np.linalg.norm(mean_color - (0, 0, 1))
   distances = [distance_green, distance_purple, distance_red]
   print distances
 
