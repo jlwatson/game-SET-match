9b6b389aa87e1b0b92976e4b849f754afe95e76b
Kent Blake <kmblake@stanford.edu>
2017-06-08 13:39:23 -0700

    Added HOG features

--- classifiers/pixel_feature_extractor.py
+++ classifiers/pixel_feature_extractor.py
@@ -2,8 +2,10 @@ import numpy as np
 from scipy import misc
 import pdb
 import math
-# from skimage.feature import daisy
-# from skimage import data, color, exposure
+from skimage.feature import hog
+from skimage import color
+# import matplotlib.pyplot as plt
+# import pdb
 
 
 class PixelFeatureExtractor:
@@ -18,14 +20,16 @@ class PixelFeatureExtractor:
 		img = misc.imread(imgfile)
 		resized = misc.imresize(img, (PixelFeatureExtractor.NUM_ROWS, PixelFeatureExtractor.NUM_COLS))
 		resized = resized[crop_radius:PixelFeatureExtractor.NUM_ROWS - crop_radius, crop_radius:PixelFeatureExtractor.NUM_COLS - crop_radius ]
-		# resized_gray = color.rgb2gray(resized)
-		# daisy_features = daisy(resized_gray)
-		# pdb.set_trace()
+		resized_gray = color.rgb2gray(resized)
+		hog_features = hog(resized_gray)
+		# plt.imshow(hog_image)
+		# plt.show()
+  	# daisy_features = daisy(resized_gray)
 
-		return np.ndarray.flatten(resized)
+		return np.append(np.ndarray.flatten(resized), np.ndarray.flatten(hog_features))
 		
 
-# Example usage
+# # Example usage
 # p = PixelFeatureExtractor()
 # p.get_features('../set_images/green_one_oval_hollow.jpg')
 
