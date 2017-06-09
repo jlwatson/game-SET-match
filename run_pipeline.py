dad7ff0aa40070e58a186c534d8007023ae95988
Kent Blake <kmblake@stanford.edu>
2017-06-08 20:50:29 -0700

    Made full pipeline test script

--- run_pipeline.py
+++ run_pipeline.py
@@ -16,25 +16,27 @@ class Pipeline:
 
   CODES = dict({'oval': 0, 'squiggle': 1, 'rhombus': 2, 'stripe': 0, 'solid': 1, 'hollow': 2, 'one': 0, 'two': 1, 'three': 2, 'green': 0, 'purple': 1, 'red': 2})
 
-  def __init__(self, card_dir="detection/output", testing=False):
+  def __init__(self, root_dir="detection/output", testing=False):
     self.classifiers = [joblib.load('pipeline/color_clf.pkl'), joblib.load('pipeline/quantity_clf.pkl'), joblib.load('pipeline/shape_clf.pkl'), joblib.load('pipeline/shade_clf.pkl') ]
-    self.card_dir = card_dir
+    self.root_dir = root_dir
     self.testing = testing
-
 
   def classify_cards(self):
     p = PixelFeatureExtractor()
     Y = []
     X = []
-    for filename in os.listdir(self.card_dir):
-      print filename
-      if filename.endswith(".jpg"):
-        if self.testing: 
-          trim_name = filename.split('.')[0]
-          labels = trim_name.split('_')
-          label_vals = [self.CODES[label] for i, label in enumerate(labels)]
-          Y.append(label_vals)
-        X.append(p.get_features(self.card_dir + '/' + filename))
+    for subdir, dirs, files in os.walk(self.root_dir):
+      for card_dir in dirs:
+        print card_dir
+        for filename in os.listdir(self.root_dir + '/' + card_dir):
+          # print filename
+          if filename.endswith(".jpg"):
+            if self.testing: 
+              trim_name = filename.split('.')[0]
+              labels = trim_name.split('_')
+              label_vals = [self.CODES[label] for i, label in enumerate(labels)]
+              Y.append(label_vals)
+            X.append(p.get_features(self.root_dir + '/' + card_dir + '/' + filename))
 
     if self.testing:
       Y = np.array(Y)
@@ -42,12 +44,21 @@ class Pipeline:
 
     # Quantity, shape, shade
     predictions = {}
-    for i in xrange(1, 4):
+    for i in xrange(0, 4):
       clf_name = self.CLASSIFIER_NAMES[i]
       clf = self.classifiers[i]
 
-      cur_predictions = clf.predict(X)
-      predictions[clf_name] = cur_predictions
+      if i == 0:
+        cur_predictions = []
+        for subdir, dirs, files in os.walk(self.root_dir):
+          for card_dir in dirs:
+            for filename in os.listdir(self.root_dir + '/' + card_dir):
+              if filename.endswith(".jpg"):
+                cur_predictions.append(get_color(self.root_dir + '/' + card_dir + '/' + filename))
+        predictions[clf_name] = cur_predictions
+      else:
+        cur_predictions = clf.predict(X)
+        predictions[clf_name] = cur_predictions
 
       if self.testing:
         f1 = f1_score(Y[:, i], cur_predictions, labels=[0, 1, 2], average='micro')
@@ -59,33 +70,20 @@ class Pipeline:
         print "F1 Score for %s: %f" % (clf_name, f1)
 
     # Color
-    clf_name = self.CLASSIFIER_NAMES[0]
-    cur_predictions = []
-    for filename in os.listdir(self.card_dir):
-      if filename.endswith(".jpg"):
-        cur_predictions.append(get_color(self.card_dir + '/' + filename))
-
-    # for i in xrange(1, 4):
-    #   clf_name = self.CLASSIFIER_NAMES[i]
-    #   clf = self.classifiers[i]
-
-    #   add_features = predictions[clf_name]
-
-    #   X = np.concatenate((X, np.array(add_features).reshape((12, 1))), axis=1)
-
     # clf_name = self.CLASSIFIER_NAMES[0]
-    # clf = self.classifiers[0]
-    # cur_predictions = clf.predict(X)
-    predictions[clf_name] = cur_predictions
+    # cur_predictions = []
     
-    if self.testing:
-        f1 = f1_score(Y[:, 0], cur_predictions, labels=[0, 1, 2], average='micro')
-        print clf_name
-        print "Predicted"
-        print cur_predictions
-        print "Actual"
-        print Y[:, 0]
-        print "F1 Score for %s: %f" % (clf_name, f1)
+
+    # predictions[clf_name] = cur_predictions
+    
+    # if self.testing:
+    #     f1 = f1_score(Y[:, 0], cur_predictions, labels=[0, 1, 2], average='micro')
+    #     print clf_name
+    #     print "Predicted"
+    #     print cur_predictions
+    #     print "Actual"
+    #     print Y[:, 0]
+    #     print "F1 Score for %s: %f" % (clf_name, f1)
 
     cards = []
     for i in xrange(len(predictions['color'])):
@@ -107,7 +105,7 @@ class Pipeline:
         label_filename = root_dir + img_name + '_labels.txt'
         if int(os.path.getsize(label_filename)) > 0:
           # labels exist and are complete
-          c.getCards(root_dir + filename, 'detection/output', card_name_file=label_filename)
+          c.getCards(root_dir + filename, 'detection/output/' + img_name, card_name_file=label_filename)
           print "Got cards for " + img_name
 
 # os.system("python detection/corner_detection.py detection/test_input/wb_on_center.jpg detection/output --deterministic --labels detection/test_input/wb_on_center_names.txt")
@@ -117,7 +115,7 @@ class Pipeline:
 
 
 
-p = Pipeline(testing=True, card_dir='detection/output')
-# p.detect_cards()
+p = Pipeline(testing=True, root_dir='detection/output')
+p.detect_cards()
 cards = p.classify_cards()
-print set_finder.find(cards)
+# print set_finder.find(cards)
